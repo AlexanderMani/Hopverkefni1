6ad88d85796d96b2ab68091109d7e405d3835671
Árni Þór Sörensen <arni@hugsnilld.is>
2020-10-29 19:54:57 +0000

    Lagaði bögg í header animation. Síðasti böggurinn lagaður.

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,25 @@
 const menuBtn = document.querySelector('.menu-btn');
+const menuBtn2 = document.querySelector('.menu-btn2');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
+    menuBtn2.classList.add('open');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    menuBtn2.classList.remove('open');
+    menuOpen = false;
+  }
+});
+menuBtn2.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuBtn2.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuBtn2.classList.remove('open');
     menuOpen = false;
   }
 });
