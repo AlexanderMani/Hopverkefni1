864b57d93d719ebfd4485dc72e568855e172d11f
Árni Þór Sörensen <arni@hugsnilld.is>
2020-10-29 20:08:02 +0000

    lokalagfæringar

--- javascript/toggle.js
+++ javascript/toggle.js
@@ -1,6 +1,9 @@
 function toggle() {
   let image = document.getElementById("checkmark");
+  let background = document.querySelector("background")
   image.classList.toggle("hide");
+  background.classList.toggle("grey-background-index");
+  background.classList.toggle("grey-background-index-checked");
   event.stopPropagation();
   return;
 }
